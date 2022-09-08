ca5e64825820ee75f5eebaf5d592511553a611e8
safwaan <safwaan.abrams@media24.com>
2022-09-08 14:52:19 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -20,17 +20,11 @@ int main(void)
 
 {
 
-	       puts("\"Programming is like building a multilingual puzzle");
+               puts("\"Programming is like building a multilingual puzzle");
 
-	              return (0);
+                      return (0);
 
 }
-
-
-
-
-
-
 
 
 
