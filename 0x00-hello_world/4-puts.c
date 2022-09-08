6754a8d86fe3f828fa5a4baca2ab7621f1ed42ab
safwaan <safwaan.abrams@media24.com>
2022-09-08 15:01:38 -0700

    nnew

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,16 +3,8 @@
 
 /**
  * main - print the string in the put function
- *
- * *
- *
- * * Description: using the main function
- *
- * * this program prints "Programming is like building a multilingual puzzle
- *
- * * Return: 0
- *
- * */
+ * Return: 0
+ */
 
 int main(void)
 
