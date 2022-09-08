54ef2944aa33eb98fb4cc2ba01e31df452bb847c
safwaan <safwaan.abrams@media24.com>
2022-09-08 15:14:52 -0700

    another new

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,40 +1,35 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - entry point
- *
- *   *
- *
- *    * Return: Always 0(Success)
- *
- *     */
+ * main - print the string in the put function
+ * Return: 0
+ */
 
 int main(void)
 
 {
 
-	int a;
+	int i;
 
-	long int b;
+	long li;
 
-	long long int c;
+	long long lli;
 
-	char d;
+	char c;
 
 	float f;
 
 
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 
 	return (0);
 
