04ef2bc616f77a0da1e25b09c43ebfe894f0a1b1
safwaan <safwaan.abrams@media24.com>
2022-09-08 15:08:11 -0700

    newnew

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * main - print the string in the put function
+ * Return: 0
+ */
 int main(void)
-
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-	        return (0);
-
-		}
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
