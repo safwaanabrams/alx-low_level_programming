c9d53859e14c4bc3e8f20f384fbb1146dbac1e1d
safwaan <safwaan.abrams@media24.com>
2022-09-09 05:24:19 -0700

    chmod +x 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ * * main - entry point
+ *
+ * * Return: Always 0 (Success/correct)
+ *
+ * */
+
+
+
+int main(void)
+
+{
+
+	int n;
+
+
+
+	srand(time(0));
+
+	n = rand() - RAND_MAX / 2;
+
+
+
+	if (n ==0)
+
+	{
+
+		printf("%i is zero\n" , n);
+
+	}
+
+
+
+	else if (n < 0)
+
+	{
+
+		printf("%i is negativr\n" , n);
+
+	}
+
+	else
+
+	{
+
+		printf("%i is positive\n" , n);
+
+	}
+
+	return (0);
+
+}
