21b9d57d8753e752f629a9d9564cd2a30d198b1c
safwaan <safwaan.abrams@media24.com>
2022-09-09 06:58:27 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success/correct)
+*/
+int main(void)
+
+{
+char base_16;
+
+for (base_16 = 48; base_16 <= 57; base_16++)
+
+{
+putchar(base_16);
+}
+
+putchar('\n');
+
+return (0);
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                                                               0,1           All
+
