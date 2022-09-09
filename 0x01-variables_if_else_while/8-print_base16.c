bc4e2825634e66d7b5d80ff6d6f0fd3bf8094142
safwaan <safwaan.abrams@media24.com>
2022-09-09 07:01:32 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,7 +2,7 @@
 /**
 * main - Entry point
 *
-* Return: Always 0 (Success/correct)
+* Return: 0 Success
 */
 int main(void)
 
@@ -10,6 +10,12 @@ int main(void)
 char base_16;
 
 for (base_16 = 48; base_16 <= 57; base_16++)
+
+{
+putchar(base_16);
+}
+
+for (base_16 = 97; base_16 <= 102; base_16++)
 
 {
 putchar(base_16);
