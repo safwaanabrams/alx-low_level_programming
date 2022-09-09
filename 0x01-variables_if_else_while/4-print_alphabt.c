a0593207af5be180871ca5f20478e108a44b1b40
safwaan <safwaan.abrams@media24.com>
2022-09-09 07:30:05 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,13 @@
 */
 int main(void)
 {
-char alphabet;
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
 {
-       if (alphabet == 'q')
-           continue;
-
-       else if (alphabet == 'e') 
-           continue;
-
-       putchar (alphabet);
+if (ch != 'e' && ch != 'q')
+{
+       putchar (ch);
+}
 }
 putchar('\n');
 return (0);
